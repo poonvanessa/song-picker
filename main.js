a6ab6ed2b978702584faaa344937187852ee9c55
echelper@ust.hk <iamvanessapoon@gmail.com>
2020-12-22 11:18:46 +0800

    add a form with two-way binding

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ Vue.component('song', {
             likes: 0,
         }
     },
-    template: `<div class="song-listItem" :key="name">
+    template: `<div class="song-wrapper" :key="name">
 <h2>{{ title }}</h2>
 <ul>
     <li>Link: <a v-if="link" :href="link">{{ link }}</a><span v-else>-</span></li>
@@ -37,6 +37,46 @@ Vue.component('song', {
     computed: {
         title() {
             return this.name + ' - ' + this.singer
+        }
+    }
+})
+Vue.component('new-song', {
+    template: `<div class="song-wrapper">
+    <h2>Add a New Song</h2>
+    <form class="song-form" @submit.prevent="onSubmit">
+        <div class="song-formInput">
+            <label for="name">Name</label>
+            <input id="name" v-model="name">
+        </div>
+        <div class="song-formInput">
+            <label for="singer">Singer</label>
+            <input id="singer" v-model="singer">
+        </div>
+        <div class="song-formInput">
+            <label for="link">Link</label>
+            <input id="link" v-model="link">
+        </div>
+        <input type="submit" value="Submit">
+    </form>
+</div>`,
+    data() {
+        return {
+            name: null,
+            singer: null,
+            link: null,
+        }
+    },
+    methods: {
+        onSubmit() {
+            let newSong = {
+                name: this.name,
+                singer: this.singer,
+                link: this.link
+            }
+            this.$emit('add-new-song', newSong)
+            this.name = null
+            this.singer = null
+            this.link = null
         }
     }
 })
@@ -74,6 +114,9 @@ var app = new Vue({
     methods: {
         addVotes(like) {
             this.votes += like
+        },
+        addSong(song) {
+            this.songs.unshift(song)
         }
     }
 })
