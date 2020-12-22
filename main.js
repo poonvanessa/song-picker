8d3977d53bbe15f58ad231f5664896c93fdcb4e8
echelper@ust.hk <iamvanessapoon@gmail.com>
2020-12-22 10:57:29 +0800

    create a page of song picker

---
+++ main.js
@@ -0,0 +1,79 @@
+Vue.component('song', {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        singer: {
+            type: String,
+            required: true,
+        },
+        link: {
+            type: String,
+            required: false,
+        },
+    },
+    // data must be a function while defining in a component
+    data: function() {
+        return {
+            likes: 0,
+        }
+    },
+    template: `<div class="song-listItem" :key="name">
+<h2>{{ title }}</h2>
+<ul>
+    <li>Link: <a v-if="link" :href="link">{{ link }}</a><span v-else>-</span></li>
+    <li>Likes: {{ likes }}</li>
+</ul>
+<button @click="addLike">Like</button>
+</div>`,
+    methods: {
+        addLike() {
+            this.likes += 1
+            this.$emit('add-like', 1)
+        }
+    },
+    // uses cache, more efficient than methods
+    computed: {
+        title() {
+            return this.name + ' - ' + this.singer
+        }
+    }
+})
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Song Picker',
+        votes: 0,
+        songs: [{
+                name: '天使と悪魔',
+                singer: '世界の終わり',
+                link: 'https://youtu.be/hVFY4Yvv3ho',
+            },
+            {
+                name: 'マリーゴールド',
+                singer: 'あいみょん',
+                link: 'https://youtu.be/0xSiBpUdW4E'
+            },
+            {
+                name: '帰り道',
+                singer: 'ＯＡＵ',
+                link: 'https://youtu.be/-HJpWz8O97Y',
+            },
+            {
+                name: '香水',
+                singer: '瑛人',
+                link: 'https://youtu.be/9MjAJSoaoSo',
+            },
+            {
+                name: '別の人の彼女になったよ',
+                singer: 'wacci',
+            }
+        ]
+    },
+    methods: {
+        addVotes(like) {
+            this.votes += like
+        }
+    }
+})
